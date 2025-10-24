90f31db583cc2bb786bb2ef9d476531aa14ff80d
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2025-10-24 19:15:32 +0530

    Update mini-al.sh

--- mini-al.sh
+++ mini-al.sh
@@ -4,7 +4,6 @@
 
 yum install docker -y
 systemctl start docker
-systemctl status docker
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo mv kubectl /usr/local/bin/kubectl
 sudo chmod +x /usr/local/bin/kubectl
@@ -13,4 +12,4 @@ sudo install minikube-linux-amd64 /usr/local/bin/minikube
 sudo yum install iptables -y
 yum install conntrack -y
 minikube start --driver=docker --force
-minikube status 
+minikube status && systemctl status docker
